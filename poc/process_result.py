d254e8ba92e9b9b9f1f64560f9e7cbf62671fd17
Hatuw <jiaxi_wu@ieee.org>
2018-03-05 00:17:18 +0800

    try to show masks

--- poc/process_result.py
+++ poc/process_result.py
@@ -6,8 +6,16 @@ Detecting nuclei and processing results.
 import os
 import sys
 sys.path.append("Mask_RCNN")
+import random
+import numpy as np
+import skimage
+import matplotlib
+import matplotlib.pyplot as plt
 
+from config import Config
+import utils
 import model as modellib
+import visualize
 
 # Root directory of the project
 ROOT_DIR = os.getcwd()
@@ -24,10 +32,134 @@ COCO_MODEL_PATH = os.path.join(ROOT_DIR, "Mask_RCNN/mask_rcnn_coco.h5")
 if not os.path.exists(COCO_MODEL_PATH):
     utils.download_trained_weights(COCO_MODEL_PATH)
 
+
+class ShapesConfig(Config):
+    """Configuration for training on the toy shapes dataset.
+    Derives from the base Config class and overrides values specific
+    to the toy shapes dataset.
+    """
+    # Give the configuration a recognizable name
+    NAME = "shapes"
+
+    # Train on 1 GPU and 8 images per GPU. We can put multiple images on each
+    # GPU because the images are small. Batch size is 8 (GPUs * images/GPU).
+    GPU_COUNT = 1
+    IMAGES_PER_GPU = 4  # 8
+
+    # Number of classes (including background)
+    # NUM_CLASSES = 1 + 3  # background + 3 shapes
+    NUM_CLASSES = 1 + 1  # background + 1 shapes
+
+    # Use small images for faster training. Set the limits of the small side
+    # the large side, and that determines the image shape.
+    IMAGE_MIN_DIM = 128
+    IMAGE_MAX_DIM = 256 # 128
+
+    # Use smaller anchors because our image and objects are small
+    RPN_ANCHOR_SCALES = (8, 16, 32, 64, 128)  # anchor side in pixels
+
+    # Reduce training ROIs per image because the images are small and have
+    # few objects. Aim to allow ROI sampling to pick 33% positive ROIs.
+    TRAIN_ROIS_PER_IMAGE = 32
+
+    # Use a small epoch since the data is simple
+    STEPS_PER_EPOCH = 200   # 100
+
+    # use small validation steps since the epoch is small
+    VALIDATION_STEPS = 10   # 5
+
+config = ShapesConfig()
+
+
+class ShapesDataset(utils.Dataset):
+    """Generates the shapes synthetic dataset. The dataset consists of simple
+    shapes (triangles, squares, circles) placed randomly on a blank surface.
+    The images are generated on the fly. No file access required.
+    """
+
+
+    def load_shapes(self, count, image_ids):
+    # def load_shapes(self, count, height, width, image_ids):
+        """Generate the requested number of synthetic images.
+        count: number of images to generate.
+        height, width: the size of the generated images.
+        """
+        # Add classes
+        self.add_class("shapes", 1, "nuclei")
+
+        if not image_ids:
+            image_ids = os.listdir(DATA_DIR)
+        for index, item in enumerate(image_ids):
+            temp_image_path = "{0}/{1}/images/{1}.png".format(DATA_DIR, item)
+            # print(temp_image_path)
+            # break
+            self.add_image("shapes", image_id=index,
+                            kaggle_id=item,
+                            path=temp_image_path)
+
+
+    def load_image(self, image_id):
+        """Generate an image from the specs of the given image ID.
+        Typically this function loads the image from a file, but
+        in this case it generates the image on the fly from the
+        specs in image_info.
+        """
+
+
+        info = self.image_info[image_id]
+        image = plt.imread(info['path'])[:,:,:3]    # some image maybe 4 channels, need to fix it
+        return image
+
+
+    def image_reference(self, image_id):
+        """Return the shapes data of the image."""
+        info = self.image_info[image_id]
+        if info["source"] == "shapes":
+            return info["shapes"]
+        else:
+            super(self.__class__).image_reference(self, image_id)
+
+
+    def load_mask(self, image_id):
+        """Generate instance masks for shapes of the given image ID.
+        """
+        info = self.image_info[image_id]
+        count = 1
+        kaggle_id = info['kaggle_id']
+        mask_dir = "{0}/{1}/masks".format(DATA_DIR, kaggle_id)
+        masks_list = os.listdir(mask_dir)
+
+        count = len(masks_list)
+        temp_img = skimage.io.imread(mask_dir + "/" + masks_list[0])
+        mask = np.zeros([temp_img.shape[0], temp_img.shape[1], len(masks_list)])
+        for index, item in enumerate(masks_list):
+            temp_mask_path = "{}/{}".format(mask_dir, item)
+            mask[:, :, index:index+1] = skimage.io.imread(temp_mask_path)[:, : , np.newaxis]
+
+        # Handle occlusions
+        occlusion = np.logical_not(mask[:, :, -1]).astype(np.uint8)
+        for i in range(count-2, -1, -1):
+            mask[:, :, i] = mask[:, :, i] * occlusion
+            occlusion = np.logical_and(occlusion, np.logical_not(mask[:, :, i]))
+
+        # class_ids = np.array([1])
+        class_ids = np.array([1 for _ in range(0, count, 1)])
+        # mask = cv2.cvtColor(mask, cv2.COLOR_GRAY2RGB)
+        return mask, class_ids.astype(np.int32)
+
+
+class InferenceConfig(ShapesConfig):
+    GPU_COUNT = 1
+    IMAGES_PER_GPU = 1
+
+inference_config = InferenceConfig()
+
+
 def detect_nuclei():
     """
     This function is used to detect nuclei by using Mask-RCNN model.
     """
+
     # Recreate the model in inference mode
     model = modellib.MaskRCNN(mode="inference",
                               config=inference_config,
@@ -49,15 +181,40 @@ def detect_nuclei():
         modellib.load_image_gt(dataset_val, inference_config,
                                image_id, use_mini_mask=False)
 
-    log("original_image", original_image)
-    log("image_meta", image_meta)
-    log("gt_class_id", gt_class_id)
-    log("gt_bbox", gt_bbox)
-    log("gt_mask", gt_mask)
+    # log("original_image", original_image)
+    # log("image_meta", image_meta)
+    # log("gt_class_id", gt_class_id)
+    # log("gt_bbox", gt_bbox)
+    # log("gt_mask", gt_mask)
 
-    visualize.display_instances(original_image, gt_bbox, gt_mask, gt_class_id,
-                            dataset_train.class_names, figsize=(8, 8))
+    fig = plt.figure()
+    imshow = np.sum(gt_mask, -1)
+    print(imshow.shape)
+    plt.imshow(imshow, cmap='gray')
+    plt.show()
+    # visualize.display_instances(original_image, gt_bbox, gt_mask, gt_class_id,
+    #                         dataset_train.class_names, figsize=(8, 8))
 
 
 def process_result():
     pass
+
+
+# split_training and testing set
+image_ids = os.listdir(DATA_DIR)
+np.random.shuffle(image_ids)
+dtset_size = len(image_ids)
+training_ids = image_ids[:int(dtset_size*0.8)]
+testing_ids = image_ids[int(dtset_size*0.8):]
+
+# Training dataset
+dataset_train = ShapesDataset()
+dataset_train.load_shapes(len(training_ids), image_ids=training_ids)
+dataset_train.prepare()
+
+# Validation dataset
+dataset_val = ShapesDataset()
+dataset_val.load_shapes(len(testing_ids), image_ids=testing_ids)
+dataset_val.prepare()
+
+detect_nuclei()
